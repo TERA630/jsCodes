bdb717a39f7b1a7c80b99d2972bfc312761b8e47
Yoshihiko <tera.bcool.4475@gmail.com>
2023-08-30 17:52:24 +0900

    2023830

--- 新型コロナウィルス感染症.js
+++ 新型コロナウィルス感染症.js
@@ -1,7 +1,7 @@
 
- const selectName = ['age','SpO2'];
- const labelForSelect = ['年齢','酸素飽和度(SpO2)','臨床状態','ワクチン接種'];
- const optionOfSelect = [['65歳未満','65歳≦<75歳','75歳以上'],['≧96','93<<96%','<93%'],['呼吸器症状なし','咳のみ､呼吸困難なし','呼吸困難､肺炎所見','酸素投与が必要','ICUor人工呼吸器が必要'],['未接種','接種済み']];
+ const selectName = ['selectAge','selectSpO2','selectOnset','selectSymptom'];
+ const labelForSelect = ['年齢','酸素飽和度(SpO2)','臨床状態','発症からの時期','ワクチン接種'];
+ const optionOfSelect = [['65歳未満','65歳≦<75歳','75歳以上'],['≧96','93<<96%','<93%'],,['3日以内','5日以内','7日以内'],['呼吸器症状なし','咳のみ､呼吸困難なし','呼吸困難､肺炎所見','酸素投与が必要','ICUor人工呼吸器が必要'],['未接種','接種済み']];
  const checkBoxName = ['malignancy','chronicRespiratoryDisease','DM','chronicLiverDiease','CKD','CVD','HT','Obesity','immuneSuppressed'];
  const labelForCheckBox = ['悪性腫瘍','慢性呼吸器疾患(COPDなど)','糖尿病','慢性肝障害','慢性腎不全','心血管･脳血管疾患','高血圧','肥満','免疫抑制'];
 
@@ -13,10 +13,46 @@ window.onload = function(){
 
 //
 function calcSeverity(){
-   
+   let GradeOfSaturation = getIntBySelect('selectSpO2');
+   let GradeOfSymptom = getIntBySelect('selectSymptom');
+   if(GradeOfSaturation ==0 && GradeOfSymptom == 0)
+   { SeverityOfCOVID = '軽症'
+   } else if ( GradeOfSaturation == 1 && GradeOfSymptom<=1){
+    
+
+
+   } 
 }
 
 
+function getObjectOrAlert(name_of_element){ // InputかSelectかオブジェクトがなければAlertする｡
+  let queryInput = document.querySelector(` input[name="${name_of_element}"]`)
+  if(queryInput==null){  // Inputでオブジェクトが見つからなかった場合
+    let querySelect = document.querySelector(`select[name="${name_of_element}"]`)
+    if(querySelect == null ) {   
+      alert(`${name_of_element} was not found. Modify html.`);
+      return null
+    } else {
+      return querySelect;
+    }
+  } else {
+  return queryInput; 
+  }
+  
+  function getIntByName(nameOfElementObject){
+  let valueInt =   getObjectOrAlert(nameOfElementObject)?.value ?? 0;
+  return parseInt(valueInt);
+  }
+
+  function getIntBySelect(nameOfElementObject){ // return : 　nameOfElementObject.options[nameOfElementObject.selectedIndex].value か0
+  let selectObject =  getObjectOrAlert(nameOfElementObject) // selectObject nullable 
+  let indexOfElement = selectObject?.selectedIndex ?? -1
+  if(indexOfElement == -1 ) {
+    return 0
+  } else {
+    return parseInt(selectObject.options[indexOfElement].value);
+  }
+  }
 
 
 function renderSelect(parentForm){ // selectName, labelForSelect,optionOfSelect
@@ -50,5 +86,6 @@ function renderCheckBox(parentForm){ // checkBoxName,labelForCheckBox
     label.htmlFor = checkBoxElement;
     label.textContent = labelForCheckBox[i];
     parentForm.insertBefore(label,null);
+}
 }
 }
